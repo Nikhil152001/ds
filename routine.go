9571a5eff924de54ac55baba96d9ac70d55c1375
Nikhil pachpute <nikhilpachpute@Nikhils-MacBook-Air.local>
2025-02-13 21:44:12 +0530

    hello world

--- routine.go
+++ routine.go
@@ -1,22 +1,21 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
-       
+// import (
+// 	"fmt"
+// 	"time"
+// )
 
-func sayHello() {
-	fmt.Println("hi nikhil")
-    time.Sleep(5000 * time.Millisecond)
-}
-func sayHi() {
-	fmt.Println("nikhil")
-}
+// func sayHello() {
+// 	fmt.Println("hi nikhil")
+//     time.Sleep(5000 * time.Millisecond)
+// }
+// func sayHi() {
+// 	fmt.Println("nikhil")
+// }
 
-func main() {
-	fmt.Println("learning go")
-	  go sayHello()
-	 go sayHi()
-    time.Sleep(6000* time.Microsecond)
-}
+// func main() {
+// 	fmt.Println("learning go")
+// 	  go sayHello()
+// 	 go sayHi()
+//     time.Sleep(6000* time.Microsecond)
+// }
