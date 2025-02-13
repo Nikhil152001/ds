06a013583d884f692167d6080b53a4102db85208
Nikhil pachpute <nikhilpachpute@Nikhils-MacBook-Air.local>
2025-02-14 00:45:39 +0530

    nik

---
+++ range.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+
+	
+)
+
+func main() {
+	   nik:= []int {11,1,1,1,1,1}
+	    for index , value := range nik {
+	   fmt.Printf("Index of nik is %d, and value is %d", index, value)
+		}
+
+	counter := 0
+	for {
+		fmt.Println("Infinite Loop")
+		counter++
+		if counter == 5 {
+			break
+		}
+
+	}
+
+}
