7e8fe1abf4db0bf7cbcba2c1a1c36c6a3f92b634
Nikhil pachpute <nikhilpachpute@Nikhils-MacBook-Air.local>
2025-02-14 01:03:19 +0530

    map

---
+++ map.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	studentGrades := make(map[string]int)
+
+	studentGrades["nikhil"] = 222
+	studentGrades["gaurav"] = 100
+	studentGrades["vijay"] = 50
+	studentGrades["bob"] = 90
+
+	fmt.Println("Marks of student:", studentGrades)
+	studentGrades["bob"] = 40
+	fmt.Println("new Marks of student:", studentGrades["bob"])
+
+}
--- range.go
+++ range.go
@@ -1,25 +1,23 @@
 package main
 
-import (
-	"fmt"
+// import (
+// 	"fmt"
+// )
 
-	
-)
+// func main() {
+// 	nik := []int{11, 1, 1, 1, 1, 1}
+// 	for index, value := range nik {
+// 		fmt.Printf("Index of nik is %d, and value is %d", index, value)
+// 	}
 
-func main() {
-	   nik:= []int {11,1,1,1,1,1}
-	    for index , value := range nik {
-	   fmt.Printf("Index of nik is %d, and value is %d", index, value)
-		}
+// 	counter := 0
+// 	for {
+// 		fmt.Println("Infinite Loop")
+// 		counter++
+// 		if counter == 5 {
+// 			break
+// 		}
 
-	counter := 0
-	for {
-		fmt.Println("Infinite Loop")
-		counter++
-		if counter == 5 {
-			break
-		}
-
-	}
+// 	}
 
 }
