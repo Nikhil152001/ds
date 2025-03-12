268b47163777bcdf8cda3e322d85f922559f6f7b
Nikhil pachpute <nikhilpachpute@Nikhils-MacBook-Air.local>
2025-03-12 16:23:00 +0530

    length of world

--- stack.go
+++ stack.go
@@ -1,25 +1,25 @@
 package main
 
-import "fmt"
+// import "fmt"
 
-func isValid(s string) bool {
-	stack := []rune{}
+// func isValid(s string) bool {
+// 	stack := []rune{}
 
-	for _, char := range s {
-		if char == '(' {
-			stack = append(stack, char)
-		} else if char == ')' {
-			if len(stack) == 0 || stack[len(stack)-1] != '(' {
-				return false
-			}
-			stack = stack[:len(stack)-1]
-		}
-	}
+// 	for _, char := range s {
+// 		if char == '(' {
+// 			stack = append(stack, char)
+// 		} else if char == ')' {
+// 			if len(stack) == 0 || stack[len(stack)-1] != '(' {
+// 				return false
+// 			}
+// 			stack = stack[:len(stack)-1]
+// 		}
+// 	}
 
-	return len(stack) == 0
-}
+// 	return len(stack) == 0
+// }
 
-func main() {
-	s := "()"
-	fmt.Println(isValid(s))
-}
+// func main() {
+// 	s := "()"
+// 	fmt.Println(isValid(s))
+// }
