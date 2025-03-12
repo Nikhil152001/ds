384fea961ccfa4be760d98f6dc0d50685207c622
Nikhil pachpute <nikhilpachpute@Nikhils-MacBook-Air.local>
2025-03-12 14:37:19 +0530

    stack

---
+++ stack.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func isValid(s string) bool {
+	stack := []rune{}
+
+	for _, char := range s {
+		if char == '(' {
+			stack = append(stack, char)
+		} else if char == ')' {
+			if len(stack) == 0 || stack[len(stack)-1] != '(' {
+				return false
+			}
+			stack = stack[:len(stack)-1]
+		}
+	}
+
+	return len(stack) == 0
+}
+
+func main() {
+	s := "()"
+	fmt.Println(isValid(s))
+}
